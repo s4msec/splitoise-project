1bd2acadab245fe7e7bec0e876f984b87522acae
Bernardo Gazola <nardi032003@proton.me>
2025-04-30 10:40:39 -0300

    Added NestJS + Hello World

--- apps/web/components/hello-world.tsx
+++ apps/web/components/hello-world.tsx
@@ -6,9 +6,26 @@ import { useToast } from "@repo/ui/hooks/use-toast";
 const HelloWorld = () => {
   const { toast } = useToast();
 
+  const handleClick = async () => {
+    const response = await fetch("http://localhost:3001/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      toast.error("Failed to connect to the backend");
+      console.log(response);
+    }
+
+    const data = await response.json();
+    toast.success(data.message);
+  };
+
   return (
     <div>
-      <Button size="lg" onClick={() => toast.success("Hello World")}>
+      <Button size="lg" onClick={handleClick}>
         Hello World
       </Button>
     </div>
